12edacaa6e5a4c93f0698507fd41dcab45926f2b
MuhireIghor <muhireighor123@gmail.com>
2024-08-17 20:55:15 +0200

    [feat]:Nuxt app configurations customised

---
+++ nuxt.config.ts
@@ -0,0 +1,24 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+
+  components:[
+    {
+      path:"~/components",
+      pathPrefix:false,
+
+    }
+
+  ],
+
+  postcss:{
+    plugins:{
+      tailwindcss:{},
+      autoprefixer: {},
+    }
+  },
+
+  modules: ["@nuxt/icon"]
+})
