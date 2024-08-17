9016d38c5cdedc26863b62e4c7c6a9c5175a26fa
MuhireIghor <muhireighor123@gmail.com>
2024-08-17 20:49:13 +0200

    [Feat]:Added the App Button component

---
+++ components/Button/index.vue
@@ -0,0 +1,12 @@
+<script setup>
+const props = defineProps({
+  text: String,
+});
+</script>
+<template>
+  <button
+    class="bg-[#0a9a35] rounded-[6px] text-white hover:cursor-pointer font-bold leading-[48px] text-center w-[180px] h-[48px]"
+  >
+    {{ text }}
+  </button>
+</template>
---
+++ components/Sidebar/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="w-[10%] bg-black p-2 h-full flex flex-col items-center justify-center  gap-y-4">
+    <div class="flex flex-col hover:bg-white hover:border-l-[#2a96fa] w-full hover:cursor-pointer py-2 group border-2 border-transparent">
+      <Icon
+        name="tabler:template"
+        style="color: white"
+        class="text-3xl mx-auto group-hover:bg-[#333333]"
+      />
+      <p class="text-white font-semibold text-sm text-center group-hover:text-[#333333]">Templates</p>
+    </div>
+    <div class="flex flex-col hover:bg-white hover:border-l-[#2a96fa] w-full hover:cursor-pointer py-2 group border-2 border-transparent">
+      <Icon
+        name="hugeicons:text"
+        style="color: white"
+        class="text-3xl mx-auto group-hover:bg-[#333333]"
+      />
+      <p class="text-white font-semibold text-sm text-center group-hover:text-[#333333]">Add Text</p>
+    </div>
+    <div class="flex flex-col hover:bg-white hover:border-l-[#2a96fa] w-full hover:cursor-pointer py-2 group border-2 border-transparent">
+      <Icon
+        name="tabler:photo"
+        style="color: white"
+        class="text-3xl mx-auto group-hover:bg-[#333333]"
+      />
+      <p class="text-white font-semibold text-sm text-center group-hover:text-[#333333]">Add Clip Art</p>
+    </div>
+
+    <div class="flex flex-col hover:bg-white hover:border-l-[#2a96fa] w-full hover:cursor-pointer py-2 group border-2 border-transparent">
+      <Icon
+        name="lucide:layers"
+        style="color: white"
+        class="text-3xl mx-auto group-hover:bg-[#333333]"
+      />
+      <p class="text-white font-semibold text-sm text-center group-hover:text-[#333333]">Layers</p>
+    </div>
+    <div class="flex flex-col hover:bg-white hover:border-l-[#2a96fa] w-full hover:cursor-pointer py-2 group border-2 border-transparent">
+      <Icon
+        name="ion:cloud-upload-outline"
+        style="color: white"
+        class="text-3xl mx-auto group-hover:bg-[#333333]"
+      />
+      <p class="text-white font-semibold text-sm text-center group-hover:text-[#333333]">Upload</p>
+    </div>
+  </div>
+</template>
