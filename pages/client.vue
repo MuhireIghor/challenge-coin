831f19065551bb1406e51363d33737421696fadd
MuhireIghor <muhireighor123@gmail.com>
2024-08-17 20:47:35 +0200

    Added the client page

---
+++ pages/client.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex flex-col h-screen bg-[#f0f0f0]">
+    <Navbar />
+    <div class="w-full flex flex-grow h-[85%]">
+      <Sidebar />
+      <div class="flex flex-col w-full gap-y-12">
+        <Preview />
+        <Locator />
+      </div>
+    </div>
+  </div>
+</template>
