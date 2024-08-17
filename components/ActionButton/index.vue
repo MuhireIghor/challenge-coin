0444ddf0aaa6e7cf9bd7b59bc5290f86111a4437
MuhireIghor <muhireighor123@gmail.com>
2024-08-17 20:52:01 +0200

    [Feat]:Added the action button component

---
+++ components/ActionButton/index.vue
@@ -0,0 +1,15 @@
+<script setup>
+const props = defineProps({
+  icon: String,
+  text: String,
+});
+</script>
+<template>
+  <button
+    class="bg-[#f3f3f3] rounded-[6px] flex flex-col items-center aspect-[1] p-1"
+  >
+    <Icon :name="icon" style="color: #333333" class="text-3xl mx-auto" />
+
+    <p>{{ text }}</p>
+  </button>
+</template>
